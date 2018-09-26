54f87225fe4848559301b322c784ee2bd7ee6355
Dani Lyn <dlyn1110@gmail.com>
2018-09-26 22:57:30 +0000

    automatically backed up by learn

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
-RSpec.describe StaticController, type: :controller do
-
-end
+# require 'rails_helper'
+#
+# RSpec.describe StaticController, type: :controller do
+#
+# end
